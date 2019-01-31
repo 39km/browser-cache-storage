5570c6e2092ac2e5d4d03687c6efd80aa7971c71
kisug <rlatjdrn1234@gmail.com>
2019-01-31 12:28:55 +0900

    :zap: delete cacheKey in meta data when cache is deleted #1

--- src/lib/delCache.ts
+++ src/lib/delCache.ts
@@ -1,4 +1,5 @@
 import {TStorageName} from '../types';
+import CacheMeta from './CacheMeta';
 
 const delCache = (storageName: TStorageName, keyPrefix: string) =>
   function del(key: string) {
@@ -7,6 +8,10 @@ const delCache = (storageName: TStorageName, keyPrefix: string) =>
         const cacheKey = `${keyPrefix}.${key}`;
         const storage = window[storageName];
         storage.removeItem(cacheKey);
+
+        const meta = new CacheMeta(keyPrefix, storage);
+        meta.delCache(cacheKey);
+
         return true;
       } catch (err) {
         return false;
